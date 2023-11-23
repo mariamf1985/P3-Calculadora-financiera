d03ce85a7eb2439bbd88e3b031280c60be37158c
mariamf1985 <mariamf1985@gmail.com>
2023-11-23 15:14:36 +0100

    numbers view on display

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,5 +1,19 @@
 //llamamos a la pantalla
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
 //llamamos a los botones
-const buttons = document.querySelectorAll()
+const buttons = document.querySelectorAll(".btn");
+
+//para que funcionen los botones al hacer click
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        //para que aparezca lo que pulsamos en la pantalla
+        const pressedButton = button.textContent;
+        if (display.textContent === "0") {     //para que se quite el "0" al introducir números
+            display.textContent = pressedButton;
+        } else {
+            display.textContent += pressedButton;
+        }
+    })
+
+})
