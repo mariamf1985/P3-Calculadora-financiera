f32f6539d910d8da6f1cdd685ce4273eeafb5229
mariamf1985 <mariamf1985@gmail.com>
2023-11-23 11:10:02 +0100

    variables added

---
+++ pages/js/variables.js
@@ -0,0 +1,9 @@
+/*AÑADIMOS LOS NODOS QUE SON INTERACTIVOS*/
+
+/*Variables del display*/
+const displayFirstValue = document.getElementById('firstValue');
+const displayLastValue = document.getElementById('lastValue');
+
+/*Variables de botones de números*/
+const numberButtons = document.querySelectorAll('number');
+const operatorButtons = document.querySelectorAll('operator');
