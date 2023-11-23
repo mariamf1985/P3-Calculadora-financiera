66623238c304bfab7b5adcc3ef6e026dbdcfb926
mariamf1985 <mariamf1985@gmail.com>
2023-11-23 21:37:19 +0100

    operations added

--- pages/js/main.js
+++ pages/js/main.js
@@ -14,7 +14,7 @@ buttons.forEach(button => {
             return; //termina la función ejecutada.
         }
         if (button.id === "delete") {      //para borrar números de uno en uno con la flecha
-            if (display.textContent.length === 1) {
+            if (display.textContent.length === 1 || display.textContent === "Error!") {
                 display.textContent = "0";
             } else {
                 display.textContent = display.textContent.slice(0, -1);
@@ -22,8 +22,18 @@ buttons.forEach(button => {
             return;
         }
 
-        if (display.textContent === "0") {     //para que se quite el "0" al introducir números
-            display.textContent = pressedButton;
+        if (button.id === "equal") {  //para que al pusar "=" nos devuelva el resultado
+            
+            try {
+            display.textContent = eval(display.textContent); //eval evalúa un conjunto de strings que tenga 
+        } catch {                                           //operaciones matemáticas en una soa string sin formato nº.
+            display.textContent = "Error!"; 
+        }
+            return;                                          
+        }
+
+        if (display.textContent === "0" || display.textContent === "Error!") {//para que se quite el "0" 
+            display.textContent = pressedButton;                              //al introducir números
         } else {
             display.textContent += pressedButton;
         }
