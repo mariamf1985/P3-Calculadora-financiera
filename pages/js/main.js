c83df203a7920a2bfbbc5d459119f03681c88317
mariamf1985 <mariamf1985@gmail.com>
2023-11-23 16:13:54 +0100

    functions for delete numbers

--- pages/js/main.js
+++ pages/js/main.js
@@ -9,6 +9,19 @@ buttons.forEach(button => {
     button.addEventListener("click", () => {
         //para que aparezca lo que pulsamos en la pantalla
         const pressedButton = button.textContent;
+        if (button.id === "C") {         //para que al pulsar "C" vuelva a "0".
+            display.textContent = "0";
+            return; //termina la función ejecutada.
+        }
+        if (button.id === "delete") {      //para borrar números de uno en uno con la flecha
+            if (display.textContent.length === 1) {
+                display.textContent = "0";
+            } else {
+                display.textContent = display.textContent.slice(0, -1);
+            }
+            return;
+        }
+
         if (display.textContent === "0") {     //para que se quite el "0" al introducir números
             display.textContent = pressedButton;
         } else {
